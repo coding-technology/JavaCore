3060973a1fdbda9d8f24304d5d2c2510bcc482c5
yanqun <157468995@qq.com>
2019-09-20 11:29:25 +0800

    jvm

---
+++ 源码/MyJVM/src/com/yanqun/TestHeap.java
@@ -0,0 +1,15 @@
+package com.yanqun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestHeap {
+    public static void main(String[] args) {
+
+        List list = new ArrayList() ;
+        while(true){
+            list.add(  new int[1024*1024]) ;
+        }
+
+    }
+}
